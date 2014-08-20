c3307172fdb28e07fcf16c370d7f3744d1502f67
Pavel Kozlov <mail@pkozlov.ru>
2014-08-20 13:16:50 +0200

    HYPE initialization fix

--- src/jquery.hypescroll.js
+++ src/jquery.hypescroll.js
@@ -13,9 +13,6 @@
 
 	// Constructor
 	function HypeScroll ( element, options ) {
-		if (typeof HYPE === "undefined" || HYPE === null) {
-			throw "HypeScroll: HYPE is not defined";
-		}
 		this.element = $(element);
 		this.options = $.extend( {}, defaults, options );
 		this._defaults = defaults;
@@ -23,33 +20,67 @@
 
 		// Initialize animation config
 
-		if (this.options.documentName !== null) {
-			this.documentName = this.options.documentName;
-			this.hypeDocument = HYPE.documents[this.documentName];
-			if (typeof this.hypeDocument !== "undefined" && this.hypeDocument !== null) {
-
-			} else {
-				throw "HypeScroll: HYPE document with documentName '" + this.documentName + "' is not found";
-			}
-		} else if (this.options.hypeDocument !== null) {
-			this.hypeDocument = this.options.hypeDocument;
-		} else {
-			throw "HypeScroll: documentName or hypeDocument is required";
-		}
-
 		this.height = this.element.height();
 		this.startpoint = this.options.offset.start;
 		this.endpoint = this.startpoint + this.height + this.options.offset.end;
-		this.duration = this.hypeDocument.durationForTimelineNamed(this.options.timeline);
 
-		// time per pixel
-		this.tpp = this.duration / (this.height + this.options.offset.end);
+		if (typeof HYPE !== "undefined" && HYPE !== null) {
+			this.init();
+		} else {
+			if("HYPE_eventListeners" in window === false) {
+				window.HYPE_eventListeners = Array();
+			}
 
-		this.init();
+			window.HYPE_eventListeners.push({
+				"type": "HypeDocumentLoad",
+				"callback": (function(_this) {
+					return function(hypeDocument, element, event) {
+						return _this.init(hypeDocument, element, event);
+					};
+				})(this)
+			});
+		}
 	}
 
 	HypeScroll.prototype = {
-		init: function () {
+		init: function (hypeDocument) {
+			if (this.options.documentName !== null) {
+				if (hypeDocument && hypeDocument.documentName() === this.options.documentName) {
+					this.documentName = this.options.documentName;
+					this.hypeDocument = hypeDocument;
+				} else {
+					this.documentName = this.options.documentName;
+					this.hypeDocument = HYPE.documents[this.documentName];
+				}
+
+				if (typeof this.hypeDocument === "undefined" || this.hypeDocument === null) {
+					throw "HypeScroll: HYPE document with documentName '" + this.documentName + "' is not found";
+				}
+			} else if (this.options.hypeDocument !== null) {
+				if (hypeDocument && this.options.hypeDocument.documentName() === hypeDocument.documentName()) {
+					this.hypeDocument = hypeDocument;
+				} else {
+					this.hypeDocument = this.options.hypeDocument;
+				}
+				this.documentName = this.hypeDocument.documentName();
+			} else {
+				throw "HypeScroll: documentName or hypeDocument is required";
+			}
+
+			setTimeout((function(_this) {
+				return function() {
+					return _this.afterInit();
+				};
+			})(this), 0);
+		},
+
+		afterInit: function () {
+			this.hypeDocument.pauseTimelineNamed(this.options.timeline);
+			this.duration = this.hypeDocument.durationForTimelineNamed(this.options.timeline);
+
+			// time per pixel
+			this.tpp = this.duration / (this.height + this.options.offset.end);
+
 			$(window).scroll((function(_this) {
 				return function() {
 					return _this.onScroll();
@@ -67,9 +98,9 @@
 				animationPosition = this.duration - (position * this.tpp);
 				this.hypeDocument.goToTimeInTimelineNamed(animationPosition, this.options.timeline);
 			} else if (window_offset < this.startpoint) {
-				this.hypeDocument.goToTimeInTimelineNamed(0, this.options.timeline);
-			} else if (window_offset > this.endpoint) {
 				this.hypeDocument.goToTimeInTimelineNamed(this.duration, this.options.timeline);
+			} else if (window_offset > this.endpoint) {
+				this.hypeDocument.goToTimeInTimelineNamed(0, this.options.timeline);
 			}
 		}
 	};
